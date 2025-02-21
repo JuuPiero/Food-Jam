7d4c70087e8fdc90c086e880fd7f5b63c2ea06a6
Tom <nguyenphamhachung@gmail.com>
2025-02-21 21:03:29 +0700

    fix fail

--- GoodJam_PlayableAds/assets/Scripts/Base/State/GameState/GameWin.ts
+++ GoodJam_PlayableAds/assets/Scripts/Base/State/GameState/GameWin.ts
@@ -1,6 +1,7 @@
 import { _decorator, Component, Node } from 'cc';
 import { GameState } from './GameState';
 import { TrackingManager } from 'db://assets/base-script/PlayableAds/Tracking/TrackingManager';
+import { GameManager } from '../../../Core/GameManager';
 const { ccclass, property } = _decorator;
 
 @ccclass("GameWin")
@@ -8,6 +9,8 @@ export class GameWin extends GameState {
     
     public enterState(): void {
         console.log('GameWin');
+                GameManager.Instance.autoShowStore.active = true;
+        
         TrackingManager.winLevel();
         this._gameManager.scheduleOnce(() => {
             this._gameManager.screenWin.forEach(screen => {
