b4c22bb76deb526ccf8dae211c2738bad7f112ae
tuanna <tuanna@ikameglobal.com>
2025-12-05 16:41:13 +0700

    feat: điều chỉnh tỷ lệ

--- GoodJam_PlayableAds/assets/Scripts/Shelf/MovingShelf.ts
+++ GoodJam_PlayableAds/assets/Scripts/Shelf/MovingShelf.ts
@@ -13,7 +13,7 @@ export class MovingShelfBoundsLimit {
     horizontalLimits: Vec2 = new Vec2(-1100 , 1100);
 
     @property(Vec2)
-    verticalLimits: Vec2 = new Vec2(-1700, 1700);
+    verticalLimits: Vec2 = new Vec2(-1300, 1500);
 }
 
 @ccclass('MovingShelf')
